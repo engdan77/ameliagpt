6ff2ba5f313721b3353ff4049d0d048fcde830c5
Daniel Engvall <daniel.engvall@ipsoft.com>
2023-07-26 11:39:21 +0200

    Cleaning up 0.0.2

--- ameliagpt/llm.py
+++ ameliagpt/llm.py
@@ -9,7 +9,6 @@ from langchain.vectorstores import FAISS
 import faiss
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain, RetrievalQAWithSourcesChain
 from langchain.llms import HuggingFaceHub
 from loguru import logger
 from .shared import shared_obj
@@ -19,10 +18,6 @@ from langchain import OpenAI
 class MyLLM:
     def __init__(self, docs_source_path: Path):
         load_dotenv()
-        # raw_text = self.get_pdf_text(docs_sources)
-        # text_chunks = self.get_text_chunks(raw_text)
-        # vectorstore = self.get_vector_store(text_chunks)
-        # self.conversation = self.get_conversation_chain(vectorstore)
         self.vector_store = None
         self.docs_source_path = docs_source_path
         self.docs_sources = []
@@ -40,54 +35,6 @@ class MyLLM:
         store = self.get_faiss_vectorstore(docs, metadatas)
         self.store_faiss_vectorstore(store)
         self.load_vectorstore()
-
-    @staticmethod
-    def get_pdf_text(src_path: Path, extensions=('pdf',)):
-        text = ""
-        if not src_path.is_dir():
-            raise RuntimeError(f'Docs dir {src_path.as_posix()} needs to be a directory')
-        for ext in extensions:
-            for file in src_path.rglob(f'*.{ext}'):
-                logger.info(f'Processing {file.as_posix()}')
-                pdf_reader = PdfReader(file.as_posix())
-                for page in pdf_reader.pages:
-                    text += page.extract_text()
-                shared_obj.loaded_docs.append(file.name)
-        return text
-
-    @staticmethod
-    def get_text_chunks(text):
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text)
-        return chunks
-
-    @staticmethod
-    def get_vector_store(text_chunks):
-        embeddings = OpenAIEmbeddings()
-        # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-        vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-        return vectorstore
-
-    # Refactored
-
-    @staticmethod
-    def get_conversation_chain(vectorstore, model_name="gpt-3.5-turbo", temperature=0.7, max_tokens=1200):
-        # llm = ChatOpenAI(model_name=model_name, temperature=temperature)
-        # # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-        # memory = ConversationBufferMemory(
-        #     memory_key='chat_history', return_messages=True)
-        # conversation_chain = ConversationalRetrievalChain.from_llm(
-        #     llm=llm,
-        #     retriever=vectorstore.as_retriever(),
-        #     memory=memory
-        # )
-        conversation_chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
-        return conversation_chain
 
     @staticmethod
     def get_pdf_content(input_file: Path) -> str:
